204e375fed37c76f4aac8f6920a312247bb4f6e0
Bogdan Gligorijević <bogdangligorijevic3@gmail.com>
2022-12-16 11:18:28 +0100

    Add new object support

--- Mod.hpp
+++ Mod.hpp
@@ -14,48 +14,51 @@ int load_mod(std::ostream* out, std::string& input, bool debug, bool optional_in
 	uint32_t version;
     *out << "Version: " << (version = reader.read<uint32_t>()) << '\n';
 
-    const uint32_t objects = reader.read<uint32_t>();
-    std::string base_id, id;
-	for (size_t i = 0; i < objects; i++)
+	for (short a = 0; a < 2; a++)
 	{
-		*out << '\n'
-             << "Original ID: " << (base_id = reader.read_string(4)) << '\n'
-             << "Modified ID: " << (id = reader.read_string(4)) << '\n';
+		const uint32_t objects = reader.read<uint32_t>();
+		std::string base_id, id;
+		for (size_t i = 0; i < objects; i++)
+		{
+			*out << '\n'
+				 << "Original ID: " << (base_id = reader.read_string(4)) << '\n'
+				 << "Modified ID: " << (id = reader.read_string(4)) << '\n';
 
-		if (version >= 3) reader.advance(4 * reader.read<uint32_t>());
+			if (version >= 3) reader.advance(4 * reader.read<uint32_t>());
 
-        if (id == "") id = base_id;
-		const uint32_t modifications = reader.read<uint32_t>();
-		for (size_t j = 0; j < modifications; j++)
-        {
-			*out << id << ": Modification ID: " << reader.read_string(4) << '\n';
-			const uint32_t type = reader.read<uint32_t>();
-			if (optional_ints)
-				*out << id << ": Level variation: " << reader.read<uint32_t>() << '\n'
-				<< id << ": Data pointer: " << reader.read<uint32_t>() << '\n';
-
-			std::string data;
-			switch (type)
+			if (id == "") id = base_id;
+			const uint32_t modifications = reader.read<uint32_t>();
+			for (size_t j = 0; j < modifications; j++)
 			{
-				case 0:
-					data = std::to_string(reader.read<int>());
-					break;
-				case 1:
-				case 2:
-					data = std::to_string(reader.read<float>());
-					break;
-				case 3:
-					data = reader.read_c_string();
-					break;
-				default:
-					*out << id << ": ERROR: Unknown data type " << type << " while loading modification table.";
-					return 13;
+				*out << id << ": Modification ID: " << reader.read_string(4) << '\n';
+				const uint32_t type = reader.read<uint32_t>();
+				if (optional_ints)
+					*out << id << ": Level variation: " << reader.read<uint32_t>() << '\n'
+					<< id << ": Data pointer: " << reader.read<uint32_t>() << '\n';
+
+				std::string data;
+				switch (type)
+				{
+					case 0:
+						data = std::to_string(reader.read<int>());
+						break;
+					case 1:
+					case 2:
+						data = std::to_string(reader.read<float>());
+						break;
+					case 3:
+						data = reader.read_c_string();
+						break;
+					default:
+						*out << id << ": ERROR: Unknown data type " << type << " while loading modification table.";
+						return 13;
+				}
+				*out << id << ": Modification data: " << data << '\n';
+				if (debug)
+					*out << id << ": DEBUG: Trail: " << reader.read_string(4) << '\n';
+				else
+					reader.advance(4);
 			}
-			*out << id << ": Modification data: " << data << '\n';
-			if (debug)
-                *out << id << ": DEBUG: Trail: " << reader.read_string(4) << '\n';
-            else
-                reader.advance(4);
 		}
 	}
 	return 0;
